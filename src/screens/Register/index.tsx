15019903baae5fd1c2b9463590eab93c1a48e04e
fernandafons <nanda.fonseca7@gmail.com>
2021-11-22 23:57:22 -0300

    add forms page

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Input } from '../../Components/Forms/Input';
+import { Button } from '../../Components/Forms/Button';
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+} from './styles';
+
+export function Register(){
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input 
+            placeholder="Name"
+          />
+          <Input 
+            placeholder="Preço"
+          />
+
+        </Fields>
+
+        <Button title="Enviar"/>
+        
+      </Form>
+    </Container>
+  )
+}
