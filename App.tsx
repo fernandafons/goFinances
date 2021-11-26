597d82f83f7ca4ab8be9eee987f87deef1191f75
fernandafons <nanda.fonseca7@gmail.com>
2021-11-26 00:08:13 -0300

    add categories page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 // import { Dashboard } from './src/screens/Dashboard';
 import {ThemeProvider} from 'styled-components';
 import theme from './src/global/styles/theme';
@@ -23,7 +23,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );
 }
