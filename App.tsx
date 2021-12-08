a52407f6d09ac1dd06c4c76d19abbe137675cb4f
fernandafons <nanda.fonseca7@gmail.com>
2021-12-08 00:20:48 -0300

    add google authentication

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes} from './src/routes/app.routes';
 import {SignIn} from './src/screens/SignIn';
+import {AuthProvider} from './src/hooks/auth';
 import {
   useFonts,
   Poppins_400Regular,
@@ -31,7 +32,10 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
+
+        <AuthProvider>
         <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
 import LogoSvg from '../../assets/logo.svg';
-
+import {UseAuth} from '../../hooks/auth';
 import { SignInSocialButton } from '../../Components/SignInSocialButton';
 
 import {
@@ -17,6 +18,17 @@ import {
 } from './styles';
 
 export function SignIn(){
+  const { signInWithGoogle } = UseAuth();
+  
+  async function handleSignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert('It was not possible to connect with Google');
+    }
+  }
+
   return(
     <Container>
       <Header>
@@ -44,6 +56,7 @@ export function SignIn(){
           <SignInSocialButton 
             title="Login with Google"
             svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
           />
           <SignInSocialButton 
             title="Login with Apple"
